2ca2ce4b73566ad6733ab0ecc6568d9028b02ca3
bf <bikehike@yahoo.com>
2026-02-06 11:46:28 -0500

    moved code to angular branch

--- js/pet/PetCache.js
+++
@@ -1,7 +0,0 @@
-angular.module('PetCache', [])
-  .factory('shelters', ['$cacheFactory', function ($cacheFactory) {
-    return $cacheFactory('shelters');
-  } ])  
-  .factory('petcache', ['$cacheFactory', function ($cacheFactory) {
-    return $cacheFactory('petcache');
-  } ]);
--- unitests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-<<<<<<< Updated upstream
-npm test
-=======
-node node_modules/jasmine-node/bin/jasmine-node --verbose $1
->>>>>>> Stashed changes
