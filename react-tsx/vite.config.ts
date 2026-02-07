7a8e09abe71ec5a5d49e31578b4f8aaf639b02ea
bf <bikehike@yahoo.com>
2026-02-07 18:04:43 -0500

    react-tsx 404

--- react-tsx/vite.config.ts
+++ react-tsx/vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+      // 2. Helps with CSP: tells Vite NOT to inject the module loader inline
+      modulePreload: {
+        polyfill: false
+      },
+      // 3. Ensures CSS is its own file and not injected as a script string
+      cssCodeSplit: true,
+    }
 })
