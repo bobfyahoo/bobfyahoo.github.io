8e50362aaae311cc48cb6640faecd1f162f62882
bf <bikehike@yahoo.com>
2026-02-07 18:47:01 -0500

    added react-tsx to public

--- react-tsx/vite.config.ts
+++ react-tsx/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './react-tsx/', // 1. Ensures all asset paths are relative, which is crucial for iframe embedding
   build: {
       // 2. Helps with CSP: tells Vite NOT to inject the module loader inline
       modulePreload: {
@@ -12,5 +12,9 @@ export default defineConfig({
       },
       // 3. Ensures CSS is its own file and not injected as a script string
       cssCodeSplit: true,
+      // This will build directly into a folder the parent index.html can see
+      // Adjust '../../react-bin' to point to your desired deployment folder
+      outDir: '../react-tsx', 
+      emptyOutDir: true, // Clears the old build before creating a new one
     }
 })
